93d4fca9877d17b389509f114ffc33ec5fb9c3e4
corgile <mashiro-e@qq.com>
2023-11-02 15:35:51 +0800

    fix

--- include/cpool/conf/config.hpp
+++ include/cpool/conf/config.hpp
@@ -11,22 +11,62 @@
 
 namespace xhl {
 	struct conn_config {
-		/// 连接参数
-		std::string url;
-		std::string tb;
+/// 连接参数
+		union _sink {
+			std::string value;
+			std::string servers;
+			std::string db_file;
+
+			_sink() : value() {};
+
+			~_sink() {}
+		} sink;
+
+		union _vtb {
+			std::string value;
+			std::string topic;
+			std::string table;
+
+			_vtb() : value() {}
+
+			~_vtb() {}
+		} vtb;
+
+		int32_t partition{0};
 		int32_t max_idle{60};
 		int32_t timeout_sec{20};
 
+		operator std::string() const {
+			return
+					std::string("conn_config{.sink=")
+							.append(this->sink.value)
+							.append(", vtb=")
+							.append(this->vtb.value)
+							.append(", .partition=")
+							.append(std::to_string(partition))
+							.append(", .max_size=")
+							.append(std::to_string(max_idle))
+							.append(", .timeout_sec=")
+							.append(std::to_string(timeout_sec))
+							.append("}");
+		}
+
+		[[nodiscard]] std::string to_string() const {
+			return this->operator std::string();
+		}
+
 		conn_config& operator=(const conn_config& other) {
 			if (this == &other) {
-				return *this; // 处理自赋值情况
+				return *this; // 防止自赋值
 			}
-			url = other.url;
-			tb = other.tb;
+			partition = other.partition; // 复制数据
 			max_idle = other.max_idle;
 			timeout_sec = other.timeout_sec;
+			sink.value = other.sink.value;
+			vtb.value = other.vtb.value;
 			return *this;
 		}
+
 	};
 
 	struct pool_config {
@@ -47,7 +87,10 @@ namespace xhl {
 		conn_config cf;
 		pool_config pf;
 
-		config(conn_config& _cf, pool_config& _pf) : pf(_pf), cf(_cf) {}
+		config(conn_config& _cf, pool_config& _pf) {
+			pf = _pf;
+			cf = _cf;
+		}
 
 		config& operator=(const config& other) {
 			if (this == &other) {
